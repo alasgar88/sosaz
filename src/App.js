27b030db1cb775e1d7ad3208e3da4e6ec1c8a38f
Isgender <isgender.detroit@gmail.com>
2022-12-22 11:18:46 +0400

    big logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Routing from './Routing';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+
 function App() {
   return (
     <>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>Home <h1>big logo</h1></div>;
 };
 
 export default Home;
